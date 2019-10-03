09cade90ed862b71f5828d39ef8113963f1b59d9
talibarbosa-hub <talita.bab@gmail.com>
2019-10-03 17:00:11 -0300

    Criado botão clear Done adicionada prioridades e correção bugs

--- js/script.js
+++ js/script.js
@@ -18,95 +18,121 @@ form.addEventListener('submit', function (e) {
     } else {
         let tarefaInputada = document.createElement('li')
         let tarefaContent = document.createElement('span')
-        let x = document.createElement('button')
-        x.setAttribute('type', "button")
-        x.innerHTML = 'x';
-        x.classList.add("xStyle")
-        x.addEventListener('click', function () {
+        let optionContent = document.createElement('span')
+        let check = document.createElement('button')
+        // let editar = document.createElement('button')
+        let select =document.getElementById('select')
+        let opcoes = select.value
+
+        // check button
+        check.setAttribute('type', "button")
+        check.innerHTML = '&#10004';
+        check.classList.add("checkStyle")
+        check.addEventListener('click', function () {
             if (tarefaContent.classList.contains('checked')) {
                 tarefaContent.classList.remove('checked')
             } else {
                 tarefaContent.classList.add('checked')
             }
         })
-        tarefaContent.textContent = tarefa;
+
+        
+        optionContent.textContent = opcoes
+        tarefaContent.textContent = tarefa
+        tarefaInputada.appendChild(optionContent)
         tarefaInputada.appendChild(tarefaContent)
-        tarefaInputada.appendChild(x)
+        tarefaInputada.appendChild(check)
         listaDeTarefas.appendChild(tarefaInputada);
         tarefaInputada.classList.add('tarefa');
-        console.log(tarefaInputada)
-        form.reset()
-
-        const btnMarcarTodos = document.getElementById('btnMarcarTodos')
-        btnMarcarTodos.addEventListener('click', function(){
-        tarefaContent.classList.add('checked') 
-
-    })
         
+
+
+        // // SWITCH CASE PARA PRIORITY
+        // switch (opcoes = true) {
+        //     case 'high':
+        //         optionContent.classList.add('.high')
+        //     break;
+        
+        //     default:
+        //         break;
+        // }
+
+
+
+
+        
+
+        // Buttton Clear done
+
+        const btnLimparFeitos = document.getElementById('btnLimparFeitos')
+        btnLimparFeitos.addEventListener('click', function (){
+            if(tarefaContent.classList.contains('checked')){
+                tarefaInputada.remove()
+            }
+        
+            form.reset()
+        })
+
         // DRAG AN DROP
-
-        tarefaInputada.setAttribute('draggable','true')
-        listaDeTarefas.setAttribute('draggable','true')
-        tarefaContent.setAttribute('draggable','true')
-        x.setAttribute('draggable','true')
-        // tarefaInputada.setAttribute('ondragover', 'allowDrop(event)')
-        // ul id listadetarefas
-        //li class tarefa
-
-        //DRAGSTART
-        let element
-        listaDeTarefas.addEventListener('dragstart',function(e){
-            console.log('drag start')
-        element = e.target.closest('.tarefa')
-        })
-
-        //DRAGOVER
-        listaDeTarefas.addEventListener('dragover', function(e){
-            e.preventDefault()
-            console.log('drag over')
-            let position = e.target.closest('.tarefa')
-            this.insertBefore(element, position)
-        })
-        //DRAGEND
-        listaDeTarefas.addEventListener('dragend',function(e){
-            element =null
-        
-        })
-
-
-
-
-
-      
-
-
-
-
-        
-
+        tarefaInputada.setAttribute('draggable', 'true')
+        listaDeTarefas.setAttribute('draggable', 'true')
+        tarefaContent.setAttribute('draggable', 'true')
+        check.setAttribute('draggable', 'true')
     }
-// Button Mark all as done   
-    
 
+
+
+    //DRAGSTART
+    let element
+    listaDeTarefas.addEventListener('dragstart', function (e) {
+        console.log('drag start')
+        element = e.target.closest('.tarefa')
+    })
+
+    //DRAGOVER
+    listaDeTarefas.addEventListener('dragover', function (e) {
+        e.preventDefault()
+        console.log('drag over')
+        let position = e.target.closest('.tarefa')
+        this.insertBefore(element, position)
+    })
+    //DRAGEND
+    listaDeTarefas.addEventListener('dragend', function (e) {
+        element = null
+    })
 })
-
-
 // Button Clear List
 
 const btnLimparLista = document.getElementById('btnLimparLista')
-btnLimparLista.addEventListener('click', function(){
-document.getElementById('listaDeTarefas').innerHTML = ""
-    
+btnLimparLista.addEventListener('click', function () {
+    document.getElementById('listaDeTarefas').innerHTML = ""
+
 })
 
+//Button Mark All Done
 
 
-// BOTÃO LIMPAR TAREFAS FEITAS OPICIONAL
+const btnMarcarTodos = document.getElementById('btnMarcarTodos')
+btnMarcarTodos.addEventListener('click', function () {
+    listaDeTarefas.childNodes.forEach((tarefa) => {
+        tarefa.childNodes[0].classList.add('checked')
+    })
 
-// let tarefasRealizadas = Array.from(document.getElementsByClassName('checked'))
-//     tarefasRealizadas.forEach(function() {
+})
 
-//     })
+// const btnLimparFeitos = document.getElementById('btnLimparFeitos')
+//        btnLimparFeitos.addEventListener('click', function (){
+//            listaDeTarefas.childNodes.forEach((tarefa)=>{
+//                console.log(tarefa)
+//                if( tarefa.childNodes[0].classList.contains('checked')){
+//                     tarefa.remove()
+//                 }
+//             })
+//         })  
+
+//EDITAR TAREFA bottun
 
 
-   
+
+//ADICIONAR ETIQUETA A TAREFA
+
